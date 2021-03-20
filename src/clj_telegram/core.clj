99d48680d8848aea7e638b6058813378144b6153
Anthony Bondarenko <abondarenko@apptopia.com>
2021-03-20 15:01:42 +0200

    initial commit

---
+++ src/clj_telegram/core.clj
@@ -0,0 +1,61 @@
+(ns clj-telegram.core
+  (:require [org.httpkit.client :as http]
+            [clojure.string :as cstr]
+            [clojure.data.json :as cjson]))
+
+(def api-url "https://api.telegram.org/bot")
+
+(defn http-post [& args]
+  @(apply http/post args))
+
+(defn- request [token action data]
+  (let [url      (str api-url token "/" action)
+        response (http-post url {:form-params data
+                                 :as          :text})
+        json     (cjson/read-str (:body response) :key-fn keyword)]
+    json))
+
+(defn get-me [token]
+  (request token "getMe" nil))
+
+(defn send-message [token chat-id text]
+  (request
+    token
+    "sendMessage"
+    {:chat_id chat-id
+     :text    text})
+  true)
+
+(defn delete-webhook [token & [drop-pending-updates]]
+  (let [drop-pending-updates (or drop-pending-updates false)]
+    (request
+      token
+      "deleteWebhook"
+      {:drop_pending_updates drop-pending-updates})))
+
+(defn get-webhook-info [token]
+  (request token "getWebhookInfo" nil))
+
+; https://core.telegram.org/bots/api#setwebhook
+(defn set-webhook [token
+                   {:keys [url
+                           certificate
+                           ip_address
+                           max_connections
+                           allowed_updates
+                           drop_pending_updates]
+                    :as   data}]
+  (request token "setWebhook" data))
+
+(defn get-updates
+  ([token] (request token "getUpdates" nil))
+  ([token offset] (request token "getUpdates" {:offset offset :limit 1})))
+
+(defn process-update [token update commands]
+  (let [m         (:message update)
+        chat-id   (-> m :chat :id)
+        text      ((or (-> m :text) (-> m :chat :type)))    ; workaround to deal with group type messages
+        update-id (:update_id update)
+        [command string] (cstr/split text #" " 2)]
+    (get-updates token (inc update-id))                     ; marking updates as processed
+    [chat-id (or (second (first (filter #(= command (first %)) commands))) ["command not found"])]))
---
+++ test/clj_telegram/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-telegram.core-test
+  (:require [clojure.test :refer :all]
+            [clj-telegram.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
